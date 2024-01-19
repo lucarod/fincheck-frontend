efd29303dc26e032ff978f7c0f9125fd5be8340a
Luca Rodrigues <luca12rodrigues@gmail.com>
2024-01-19 18:10:15 -0300

    feat: add dashboard basic html add accounts and transactions components add icons

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,11 +1,23 @@
-import { useAuth } from 'src/app/hooks/useAuth';
+import { UserMenu } from 'src/view/components/UserMenu';
+import { Logo } from 'src/view/components/icons/Logo';
+import { Accounts } from './components/Accounts';
+import { Transactions } from './components/Transactions';
 
 export function Dashboard() {
-  const { signout } = useAuth();
   return (
-
-    <h1>
-      <button onClick={signout}>sair</button>
-    </h1>
+    <div className="h-full w-full flex flex-col gap-4 p-4 lg:px-8 lg:pb-8 lg:pt-6">
+      <header className="h-12 flex items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
+      <main className="flex-1 flex gap-4 flex-col lg:flex-row">
+        <section className="w-full lg:w-1/2">
+          <Accounts />
+        </section>
+        <section className="w-full lg:w-1/2">
+          <Transactions />
+        </section>
+      </main>
+    </div>
   );
 }
