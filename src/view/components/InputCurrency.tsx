1a2b89f1573c6841fad217a95eb5bf579e97033b
Luca Rodrigues <luca12rodrigues@gmail.com>
2024-02-04 21:22:33 -0300

    fix: fix input currency behavior

--- src/view/components/InputCurrency.tsx
+++ src/view/components/InputCurrency.tsx
@@ -4,23 +4,23 @@ import { InputErrorLabel } from './InputErrorLabel';
 
 interface InputCurrencyProps {
   error?: string;
-  onChange?: (value: string) => void;
-  value?: string;
+  value?: string | number;
+  onChange?: (value?: number) => void;
 }
 
 export function InputCurrency({ error, onChange, value }: InputCurrencyProps) {
   return (
     <div>
       <NumericFormat
-        onChange={event => onChange?.(event.target.value)}
-        thousandSeparator="."
-        decimalSeparator=","
         value={value}
         className={cn(
           'w-full text-gray-800 text-[32px] font-bold tracking-[-1px]',
           'outline-none h-8',
           error && 'text-red-900'
         )}
+        thousandSeparator="."
+        decimalSeparator=","
+        onValueChange={(values) => onChange?.(values.floatValue)}
       />
 
       <InputErrorLabel error={error} />
--- src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
+++ src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const schema = z.object({
-  initialBalance: z.string().min(1, 'Saldo inicial é obrigatório'),
+  initialBalance: z.number().min(1, 'Saldo inicial é obrigatório'),
   name: z.string().min(1, 'Nome da conta é obrigatório'),
   type: z.enum(['INVESTMENT', 'CASH', 'CHECKING']),
   color: z.string().min(1, 'Cor da conta é obrigatório'),
@@ -16,10 +16,10 @@ export function useNewAccountModalController() {
   const { isNewAccountModalOpen, closeNewAccountModal } = useDashboard();
 
   const {
-    control,
     register,
     handleSubmit: hookFormSubmit,
     formState: { errors },
+    control,
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
